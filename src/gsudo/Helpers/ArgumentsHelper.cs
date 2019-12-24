35cca3cb6c5d1df4051d9af00630eaa199981972
Gerardo Grignoli <gerardo.grignoli@emerios.com>
2019-12-24 11:17:30 -0300

    Allow to elevate powershell commands. (use Powershell as a shell if invoked from powershell)

--- src/gsudo/Helpers/ArgumentsHelper.cs
+++ src/gsudo/Helpers/ArgumentsHelper.cs
@@ -11,45 +11,58 @@ namespace gsudo.Helpers
 {
     class ArgumentsHelper
     {
-        static readonly HashSet<string> CMD_COMMANDS = new HashSet<string>(StringComparer.OrdinalIgnoreCase) { "ASSOC", "ATTRIB", "BREAK", "BCDEDIT", "CACLS", "CALL", "CD", "CHCP", "CHDIR", "CHKDSK", "CHKNTFS", "CLS", /*"CMD",*/ "COLOR", "COMP", "COMPACT", "CONVERT", "COPY", "DATE", "DEL", "DIR", "DISKPART", "DOSKEY", "DRIVERQUERY", "ECHO", "ENDLOCAL", "ERASE", "EXIT", "FC", "FIND", "FINDSTR", "FOR", "FORMAT", "FSUTIL", "FTYPE", "GOTO", "GPRESULT", "GRAFTABL", "HELP", "ICACLS", "IF", "LABEL", "MD", "MKDIR", "MKLINK", "MODE", "MORE", "MOVE", "OPENFILES", "PATH", "PAUSE", "POPD", "PRINT", "PROMPT", "PUSHD", "RD", "RECOVER", "REM", "REN", "RENAME", "REPLACE", "RMDIR", "ROBOCOPY", "SET", "SETLOCAL", "SC", "SCHTASKS", "SHIFT", "SHUTDOWN", "SORT", "START", "SUBST", "SYSTEMINFO", "TASKLIST", "TASKKILL", "TIME", "TITLE", "TREE", "TYPE", "VER", "VERIFY", "VOL", "XCOPY", "WMIC" };
         internal static string[] AugmentCommand(string[] args)
         {
-            if (args.Length == 0)
+            // Is our current shell Powershell ? (Powershell.exe -calls-> gsudo)
+            var parentProcess = Process.GetCurrentProcess().ParentProcess();
+            var parentExeName = Path.GetFileName(parentProcess.MainModule.FileName).ToUpperInvariant();
+            if (parentExeName == "POWERSHELL.EXE")
             {
-                // If zero args specified, Try to "elevate the current shell".
-                // Which is impossible. So we launch the current shell elevated.
-
-                // Is our current shell Powershell ? (Powershell.exe -calls-> gsudo)
-                var parentProcess = Process.GetCurrentProcess().ParentProcess();
-                var parentExeName = Path.GetFileName(parentProcess.MainModule.FileName).ToUpperInvariant();
-                if (parentExeName == "POWERSHELL.EXE")
-                    return new string[] { parentProcess.MainModule.FileName };
-
+                if (args.Length == 0)
+                    return new string[]
+                        { parentProcess.MainModule.FileName };
+                else
+                    return new string[] // Escape " => ""
+                        { parentProcess.MainModule.FileName , $"\"{ String.Join(" ", args.Select(arg => arg.Replace("\"","\"\""))) }\""};
+            }
+            else
+            {
                 // Is our current shell Powershell Core? (Pwsh.exe -calls-> dotnet -calls-> gsudo)
                 var grandParentProcess = parentProcess.ParentProcess();
                 if (grandParentProcess != null)
                 {
                     var grandParentExeName = Path.GetFileName(grandParentProcess.MainModule.FileName).ToUpperInvariant();
                     if (grandParentExeName == "PWSH.EXE")
-                        return new string[] { grandParentProcess.MainModule.FileName };
+                    {
+                        if (args.Length == 0)
+                            return new string[]
+                                { grandParentProcess.MainModule.FileName };
+                        else
+                            return new string[] // Escape " => `"
+                                { grandParentProcess.MainModule.FileName , $"\"{ String.Join(" ", args.Select(arg => arg.Replace("\"","'\""))) }\""};
+                    }
                 }
-
-                // Default, our current shell is CMD.
-                return new string[] { Environment.GetEnvironmentVariable("COMSPEC"), "/k" };
             }
 
-            if (CMD_COMMANDS.Contains(args[0]))
+            // Not Powershell, or Powershell Core, assume CMD.
+            if (args.Length == 0)
+            {
+                // If zero args specified, elevate CMD.
+
+                // Default, our current shell is CMD.
+                return new string[]
+                    { Environment.GetEnvironmentVariable("COMSPEC"), "/k" };
+            }
+            else
             {
                 // add "CMD /C" prefix to commands such as MD, CD, DIR..
                 // We are sure this will not be an interactive experience, 
                 // so we can safely use raw 
 
-                return new string[] 
+                return new string[]
                     { Environment.GetEnvironmentVariable("COMSPEC"), "/c" }
                     .Concat(args).ToArray();
             }
-
-            return args;
         }
 
         internal static int? ParseCommonSettings(ref string[] args)
