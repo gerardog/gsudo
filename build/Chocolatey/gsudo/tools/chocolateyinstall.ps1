0d37c6c29428420b7757b79f40c8da2ee6ba6e72
Gerardo Grignoli <gerardog@gmail.com>
2020-02-04 02:04:50 -0300

    Fixed Chocolatey installer. Delete symlinks created by versions 0.5.3 and below

--- build/Chocolatey/gsudo/tools/chocolateyinstall.ps1
+++ build/Chocolatey/gsudo/tools/chocolateyinstall.ps1
@@ -1,4 +1,22 @@
-﻿$bin = "$env:ChocolateyInstall\lib\gsudo\bin\"
+﻿function Test-IsSymLink([string]$path) {
+  $file = Get-Item $path -Force -ea SilentlyContinue
+  return [bool]($file.Attributes -band [IO.FileAttributes]::ReparsePoint)
+}
+
+$bin = "$env:ChocolateyInstall\lib\gsudo\bin\"
 Install-ChocolateyPath -PathToInstall $bin -PathType 'User'
 #Copy-Item -path "$bin\gsudo.exe" "$bin\sudo.exe"
-cmd /c mklink "$bin\sudo.exe" "$bin\gsudo.exe"
+if (!Test-Path "$bin\sudo.exe")
+{
+  cmd /c mklink "$bin\sudo.exe" "$bin\gsudo.exe"
+}
+
+if (Test-Path "$env:ChocolateyInstall\bin\gsudo.exe")  # Previous installers created symlinks on chocolatey\bin, we no longer need them.
+{ 
+  Remove-Item "$env:ChocolateyInstall\bin\gsudo.exe"
+  if (Test-IsSymLink "$env:ChocolateyInstall\bin\sudo.exe")
+  {
+    Remove-Item "$env:ChocolateyInstall\bin\sudo.exe"
+  }
+}
+
