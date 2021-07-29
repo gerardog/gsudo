abb6c12261105955ebc77f4c7b843f1fd076100d
Gerardo Grignoli <gerardog@gmail.com>
2021-07-29 19:34:50 -0300

    Fixed build

--- src/gsudo/Rpc/NamedPipeServer.cs
+++ src/gsudo/Rpc/NamedPipeServer.cs
@@ -19,6 +19,7 @@ namespace gsudo.Rpc
         private readonly DateTime _allowedExeTimeStamp;
         private readonly long _allowedExeLength;
         private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        private FileStream _exeLock;
 
         public event EventHandler<Connection> ConnectionAccepted;
         public event EventHandler<Connection> ConnectionClosed;
