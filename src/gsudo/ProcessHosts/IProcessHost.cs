2dfa84b8aabf937c2adc4a4a3569b33937f01f8c
Gerardo Grignoli <gerardog@gmail.com>
2021-07-30 02:10:37 -0300

    Reworded IProcessHost and IProcessRenderer comments

--- src/gsudo/ProcessHosts/IProcessHost.cs
+++ src/gsudo/ProcessHosts/IProcessHost.cs
@@ -4,9 +4,12 @@ using System.Threading.Tasks;
 namespace gsudo.ProcessHosts
 {
     /// <summary>
-    /// An AppHost starts a process, captures its output and sends it to the connection,
-    /// using a custom data and control communication protocol.
-    /// Also manages Ctrl-C forwarding from the connection to the process.    
+    /// To bridge both elevated and non-elevated worlds, 
+    /// the "Host" is the elevated part and communicates to the non-elevated "Renderer".
+    /// E.g.: For piped or VT mode, the Host starts an (elevated process, 
+    ///    captures its output and sends it (via the connection) to the Renderer,
+    ///    using a custom data and control communication protocol.
+    ///    Also manages Ctrl-C forwarding from the connection to the process.    
     /// </summary>    
     interface IProcessHost
     {
--- src/gsudo/ProcessRenderers/IProcessRenderer.cs
+++ src/gsudo/ProcessRenderers/IProcessRenderer.cs
@@ -3,10 +3,13 @@
 namespace gsudo.ProcessRenderers
 {
     /// <summary>
-    /// A Process Renderer receives I/O from a remote process and shows in the current console,
-    /// using custom data/control protocol.
-    /// Also manages Ctrl-C capturing and forwarding to the connection.
+    /// To bridge both elevated and non-elevated worlds, 
+    /// the "Host" is the elevated part and communicates to the non-elevated "Renderer".
+    /// E.g.: For piped or VT mode, a renderer receives I/O from a remote process 
+    ///   and shows it in the current console, using custom data/control protocol.
+    ///   Also manages Ctrl-C capturing and forwarding to the connection.
     /// </summary>
+    /// <remarks>This code runs in the non-elevated gsudo instance.</remarks>
     interface IProcessRenderer
     {
         Task<int> Start();
