5af1ec9dab1d999da2b8344582adb0a61b3bc558
Gerardo Grignoli <gerardog@gmail.com>
2021-08-12 18:12:08 -0300

    gsudo !! for PowerShell (closes #44)

--- src/gsudo.extras/gsudo.ps1
+++ src/gsudo.extras/gsudo.ps1
@@ -1,5 +1,13 @@
 # PowerShell Shim: Exists only to prevent PowerShell from running "gsudo" bash script.
-if($myinvocation.expectingInput) 
-	{ $input | & gsudo.exe @args } 
-else 
-	{ & gsudo.exe @args }
+
+if ($MyInvocation.Line -match "!!$")
+{ 
+	$c = Get-Content (Get-PSReadLineOption).HistorySavePath | Select-Object -last 1 -Skip 1
+	gsudo $c
+}
+elseif($myinvocation.expectingInput) {
+	$input | & gsudo.exe @args 
+} 
+else { 
+	& gsudo.exe @args 
+}
