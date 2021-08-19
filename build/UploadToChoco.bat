ccf2c9ccff7f3dedefa7991cf2bfd5fe12b2ef18
Gerardo Grignoli <gerardog@gmail.com>
2021-08-18 21:15:58 -0300

    Build scripts update

--- build/UploadToChoco.bat
+++ build/UploadToChoco.bat
@@ -1,6 +1,7 @@
 @Echo off
 
 pushd %~dp0\..
+set REPO_ROOT_FOLDER=%cd%
 
 :: Determine Version
 gitversion /showvariable LegacySemVer > "%temp%\version.tmp"
@@ -8,9 +9,9 @@ SET /P version= < "%temp%\version.tmp"
 set OUTPUT_FOLDER=%REPO_ROOT_FOLDER%\Build\Releases\%version%
 
 popd
-@pushd %OUTPUT_FOLDER%
-
-@echo Uploading v%version% to chocolatey
+pushd %OUTPUT_FOLDER%
+echo Uploading v%version% to chocolatey
+echo on
 choco push gsudo.%version%.nupkg
 
 @popd
