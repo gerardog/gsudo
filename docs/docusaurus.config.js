858ed7fea142c3ba4c5c3e93368564ada25e15ca
Gerardo Grignoli <gerardog@gmail.com>
2022-05-19 09:20:49 -0300

    Added search to docs website via Algolia.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -131,6 +131,29 @@ const config = {
         darkTheme: darkCodeTheme,
         additionalLanguages: ['powershell'],
       },
+	  algolia: {
+      // The application ID provided by Algolia
+      appId: '48THU2IKHD',
+
+      // Public API key: it is safe to commit it
+      apiKey: '062d00b0a8d02c9951557954935de46e',
+
+      indexName: 'gsudo',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      //... other Algolia params
+    },
     }),
 };
 
