951aade1297aba8f213a7cfb0916c572ebca5982
Gerardo Grignoli <gerardog@gmail.com>
2022-10-19 01:55:49 -0300

    Help updated with v1.8.0 features

--- src/gsudo/Commands/HelpCommand.cs
+++ src/gsudo/Commands/HelpCommand.cs
@@ -26,21 +26,30 @@ namespace gsudo.Commands
         internal static void ShowHelp()
         {
             ShowVersion(false);
+
+            /*
+ -h | --help\t\tShows this help
+ -v | --version\t\tShows gsudo version
+             */
+
             Console.WriteLine(@"
 Usage:
  gsudo [options]\t\t\tElevates your current shell
  gsudo [options] {command} [args]\tRuns {command} with elevated permissions
+ gsudo cache [on | off | help] \t\tStarts/Stops an elevated cache session. (reduced UAC popups)
+ gsudo status\t\t\t\tShows current user, cache and console status.
+ gsudo !!\t\t\t\tRe-run last command as admin. (YMMV)
 
 General options:
  -n | --new             Starts the command in a new console (and returns immediately).
  -w | --wait            When in new console, wait for the command to end.
 
 Security options:
- -i | --integrity {v}   Specify integrity level: Untrusted, Low, Medium, MediumPlus, High (default), System
- -k | --reset-timestamp Kills all cached credentials. The next time gsudo is run a UAC popup will be appear.
- -u | --user {username} Run as the specified username. Asks for password. For admins, shows UAC unless '-i Medium'.
+ -i | --integrity {v}   Run with integrity level: Untrusted, Low, Medium, MediumPlus, High (default), System
+ -u | --user {username} Run as the specified user. Asks for password. For local admins shows UAC unless '-i Medium'
  -s | --system          Run as Local System account (NT AUTHORITY\SYSTEM).
  --ti                   Run as member of NT SERVICE\TrustedInstaller group
+ -k | --reset-timestamp Kills all cached credentials. The next time gsudo is run a UAC popup will be appear.
 
 Shell related options:
  -d | --direct          Skip Shell detection. Asume CMD shell or CMD {command}.
@@ -50,8 +59,6 @@ Other options:
  --debug                Enable debug mode.
  --copyns               Connect network drives to the elevated user. Warning: Interactive asks for credentials
  --copyev               (deprecated) Copy all environment variables to the elevated process.
- -h | --help\t\tShows this help
- -v | --version\t\tShows gsudo version
 
 Configuration:
  gsudo config\t\t\t\tShow current config settings & values.
@@ -59,20 +66,11 @@ Configuration:
  gsudo config {key} [--global] --reset \tReset config to default value
  --global\t\t\t\tAffects all users (overrides user settings)
 
- gsudo status\t\t\t\tShows current user, cache and console status.
- gsudo cache [on | off | help] \t\tStarts/Stops an elevated cache session. (reduced UAC popups)
- gsudo !!\t\t\t\tRe-run last command as admin. (YMMV)
+From PowerShell: 
+ gsudo [options] [--loadProfile] [{ ScriptBlock }] -args $argument1, $argumentN
+    { ScriptBlock }\tMust be wrapped in { curly brackets }
+    --loadProfile\tWhen elevating PowerShell commands, load user profile.
 
-PowerShell: 
-  gsudo [options] { Write-Output @args } -args ""Hello"", ""World"" 
-  Wrap the script block in { curly brackets }
-  --loadProfile          When elevating PowerShell commands, load user profile.
-
-Warning: A running instance of gsudo can be hijacked by unprivileged software that runs on the same desktop.
-Warning: A malicious process running on the same desktop may escalate privileges hacking a running gsudo instance.
-Warning: Unprivileged software running on the same desktop may escalate privileges hacking a running gsudo instance.
-
-Learn about security considerations of using gsudo at: https://gerardog.github.io/gsudo/docs/security
 Learn more about security considerations of using gsudo at: https://gerardog.github.io/gsudo/docs/security
 "
 
