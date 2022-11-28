a9d93ac7faec1b69909b4c13bab8ad2328335bbf
Gerardo Grignoli <gerardog@gmail.com>
2022-11-28 13:50:04 -0300

    chore: build output, show which build is in progress

--- build/01-build.ps1
+++ build/01-build.ps1
@@ -1,6 +1,9 @@
 pushd $PSScriptRoot\..
+"-- Building net7.0 win-arm64"
 dotnet publish .\src\gsudo\gsudo.csproj -c Release -o .\artifacts\arm64  -f net7.0 -r win-arm64 --sc -p:PublishReadyToRun=true -p:PublishSingleFile=true -v minimal -p:WarningLevel=0 || $(exit $LASTEXITCODE)
+"-- Building net7.0 win-x86"
 dotnet publish .\src\gsudo\gsudo.csproj -c Release -o .\artifacts\x86    -f net7.0 -r win-x86   --sc -p:PublishReadyToRun=true -p:PublishSingleFile=true -v minimal -p:WarningLevel=0 || $(exit $LASTEXITCODE)
+"-- Building net7.0 win-x64"
 dotnet publish .\src\gsudo\gsudo.csproj -c Release -o .\artifacts\x64    -f net7.0 -r win-x64   --sc -p:PublishAot=true -p:IlcOptimizationPreference=Size || $(exit $LASTEXITCODE)
 
 cp .\src\gsudo.Wrappers\* .\artifacts\x86
