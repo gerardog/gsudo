01ab586a03946ff3cc192dc7be37bd47a452375b
Gerardo Grignoli <gerardog@gmail.com>
2023-02-17 17:23:05 -0300

    Re added net46-anycpu build/01-build.ps1

--- build/01-build.ps1
+++ build/01-build.ps1
@@ -7,9 +7,11 @@ dotnet publish .\src\gsudo\gsudo.csproj -c Release -o .\artifacts\arm64  -f net7
 dotnet publish .\src\gsudo\gsudo.csproj -c Release -o .\artifacts\x64    -f net7.0 -r win-x64   --sc -p:PublishAot=true -p:IlcOptimizationPreference=Size -v minimal -p:WarningLevel=0 || $(exit $LASTEXITCODE)
 "-- Building net7.0 win-x86"
 dotnet publish .\src\gsudo\gsudo.csproj -c Release -o .\artifacts\x86    -f net7.0 -r win-x86   --sc -p:PublishReadyToRun=true -p:PublishSingleFile=true -v minimal -p:WarningLevel=0 || $(exit $LASTEXITCODE)
-
+"-- Building net4.6 AnyCpu"
+dotnet publish .\src\gsudo\gsudo.csproj -c Release -o .\artifacts\net46-AnyCpu\unmerged -f net46 -p:Platform=AnyCpu -v minimal -p:WarningLevel=0 || $(exit $LASTEXITCODE)
 cp .\src\gsudo.Wrappers\* .\artifacts\x86
 cp .\src\gsudo.Wrappers\* .\artifacts\x64
 cp .\src\gsudo.Wrappers\* .\artifacts\arm64
+cp .\src\gsudo.Wrappers\* .\artifacts\net46-AnyCpu
 
 popd
