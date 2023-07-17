3c194bb2e08834b388bc3cb78ba4b11cc87a57c1
Gerardo Grignoli <gerardog@gmail.com>
2023-07-17 00:25:10 -0300

    Fix ChocolateyInstall module installer

--- build/Chocolatey/gsudo/tools/chocolateyinstall.ps1
+++ build/Chocolatey/gsudo/tools/chocolateyinstall.ps1
@@ -32,7 +32,7 @@ cmd /c mklink "$TargetDir\sudo.exe" "$TargetDir\gsudo.exe" 2>$null
 # Copy gsudoModule to "$env:ProgramFiles\PowerShell\Modules\gsudoModule"
 $PSModulesTargetDir = "$env:ProgramFiles\PowerShell\Modules\gsudoModule"
 md $PSModulesTargetDir -ErrorAction SilentlyContinue
-copy "$bin\*.ps*" $TargetDir -Exclude *.ignore -Force
+copy "$bin\*.ps*" $PSModulesTargetDir -Exclude *.ignore -Force
 
 $OldCurrentDir = Get-Item $SymLinkDir -ErrorAction ignore
 if ($OldCurrentDir) 
@@ -45,16 +45,16 @@ cmd /c mklink /d "$SymLinkDir" "$TargetDir\"
 # gsudo powershell module banner.
 "";
 
-Write-Output "gsudo successfully installed. Please restart your consoles to use gsudo.`n"
-
 if (Get-Module gsudoModule) {
 	"Please restart all your PowerShell consoles to update PowerShell gsudo Module."
 } else {
 	& { 
-	"PowerShell users: Add auto-complete to  gsudo by adding the following line to your `$PROFILE"
+	"PowerShell users: Add auto-complete to gsudo by adding the following line to your `$PROFILE"
 	"  Import-Module 'gsudoModule'"
 	"Or run: "
 	"  Write-Output `"``nImport-Module 'gsudoModule'`" | Add-Content `$PROFILE"
 
 	} 
 }
+
+Write-Output "gsudo successfully installed. Please restart your consoles to use gsudo.`n"
