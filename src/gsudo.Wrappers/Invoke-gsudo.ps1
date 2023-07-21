9ad37275c445e166f6a7e12a72295d30167f4ed3
Gerardo Grignoli <gerardog@gmail.com>
2023-07-20 23:36:40 -0300

    Rewrite of Invoke-gsudo with a significant performance improvement.

--- src/gsudo.Wrappers/Invoke-gsudo.ps1
+++ src/gsudo.Wrappers/Invoke-gsudo.ps1
@@ -1,209 +1,199 @@
 ﻿<#
 .SYNOPSIS
-Executes a ScriptBlock in a new elevated instance of powershell, using `gsudo`.
+Executes a ScriptBlock or script file in a new elevated instance of PowerShell using gsudo.
 
 .DESCRIPTION
-Serializes a scriptblock and executes it in an elevated powershell. 
-The ScriptBlock runs in a different process, so it can´t read/write variables from the invoking scope.
-If you reference a variable in a scriptblock using the `$using:variableName` it will be replaced with it´s serialized value.
-The elevated command can accept input from the pipeline with $Input. It will be serialized, so size matters.
-The command result is serialized, sent back to the non-elevated instance, deserealized and returned.
+This cmdlet serializes a ScriptBlock or script file and executes it in an elevated PowerShell instance using gsudo. The elevated command runs in a separate process, which means it cannot directly access or modify variables from the invoking scope. 
 
-Optionally you can check for "$LastExitCode -eq 999" to find out if gsudo failed to elevate (for example, UAC popup cancelled) 
+The cmdlet supports passing arguments to the ScriptBlock or script file using the -ArgumentList parameter, which can be accessed with the $args automatic variable within the ScriptBlock or script file. Additionally, you can provide input from the pipeline using $Input, which will be serialized and made available within the elevated execution context.
+
+The result of the elevated command is serialized, sent back to the non-elevated instance, deserialized, and returned. This means object graphs are recreated as PSObjects.
+
+Optionally, you can check for "$LastExitCode -eq 999" to determine if gsudo failed to elevate, such as when the UAC popup is cancelled.
 
 .PARAMETER ScriptBlock
-Specifies a ScriptBlock that will be run in an elevated PowerShell instance. '
-e.g. { Get-Process Notepad }
+Specifies a ScriptBlock to be executed in an elevated PowerShell instance. For example: { Get-Process Notepad }
+
+.PARAMETER FilePath
+Specifies the path to a script file to be executed in an elevated PowerShell instance.
 
 .PARAMETER ArgumentList
-An list of elements that will be accesible inside the script as: $args[0] ... $args[n]
+Provides a list of elements that will be accessible inside the ScriptBlock or script as $args[0], $args[1], and so on.
 
 .PARAMETER LoadProfile
-Load the user profile in the elevated powershell instance. (regardless of `gsudo config PowerShellLoadProfile`)
+Loads the user profile in the elevated PowerShell instance, regardless of the gsudo configuration setting PowerShellLoadProfile.
 
 .PARAMETER NoProfile
-Do not load the user profile in the elevated powershell instance. (regardless of `gsudo config PowerShellLoadProfile`)
+Does not load the user profile in the elevated PowerShell instance.
+
+.PARAMETER RunAsTrustedInstaller
+Runs the command as the TrustedInstaller user.
+
+.PARAMETER RunAsSystem
+Runs the command as the SYSTEM user.
+
+.PARAMETER ClearCache
+Clears the gsudo cache before executing the command.
+
+.PARAMETER NewWindow
+Opens a new window for the elevated command.
+
+.PARAMETER KeepNewWindowOpen
+Keeps the new window open after the elevated command finishes execution.
+
+.PARAMETER KeepShell
+Keeps the shell open after the elevated command finishes execution.
+
+.PARAMETER NoKeep
+Closes the shell after the elevated command finishes execution.
+
+.PARAMETER InputObject
+You can pipe any object to this function. The object will be serialized and available as $Input within the ScriptBlock or script.
 
 .INPUTS
-You can pipe any object to Invoke-Gsudo. It will be serialized and available in the userScript as $Input.
+System.Object
 
 .OUTPUTS
-Whatever the scriptblock returns.
+The output of the ScriptBlock or script executed in the elevated context.
 
 .EXAMPLE
-PS> Get-Process notepad | Invoke-gsudo { Stop-Process }
+Invoke-gsudo { return Get-Content 'C:\My Secret Folder\My Secret.txt' }
 
-PS> Invoke-gsudo { return Get-Content 'C:\My Secret Folder\My Secret.txt'}
+.EXAMPLE
+Get-Process notepad | Invoke-gsudo { $input | Stop-Process }
 
-PS> $a=1; $b = Invoke-gsudo { $using:a+10 }; Write-Host "Sum returned: $b";
+.EXAMPLE
+$a = 1; $b = Invoke-gsudo { $args[0] + 10 } -ArgumentList $a; Write-Host "Sum returned: $b"
 Sum returned: 11
+
+.EXAMPLE 
+Invoke-gsudo { Get-Process explorer } | ForEach-Object { $_.Id }
 
 .LINK
 https://github.com/gerardog/gsudo
-
-    #>
-[CmdletBinding(DefaultParameterSetName = 'None')]
+#>
+[CmdletBinding(DefaultParameterSetName='ScriptBlock')]
 param
 (
     # The script block to execute in an elevated context.
-    [Parameter(Mandatory = $true, Position = 0)]
-    [System.Management.Automation.ScriptBlock]
-    $ScriptBlock,
+    [Parameter(Mandatory = $true, Position = 0, ParameterSetName='ScriptBlock')] [System.Management.Automation.ScriptBlock]
+[ArgumentCompleter( { param ()
+            # Auto complete with last 5 ran commands.
+            $lastCommands = Get-History | Select-Object -last 5 | % { "{ $($_.CommandLine) }" }
+        
+            if ($lastCommands -is [System.Array]) {
+                # Last one first.
+                $lastCommands[($lastCommands.Length - 1)..0] | % { $_ };
+            }
+            elseif ($lastCommands) {
+                # Only one command.
+                $lastCommands;
+            }
+        } )]
 
-    # Optional argument list for the program or the script block.
-    [Parameter(Mandatory = $false, Position = 1)]
-    [System.Object[]]
-    $ArgumentList,
+        $ScriptBlock,
 
-    [Parameter(ValueFromPipeline)]
-    [pscustomobject]
-    $InputObject,
+    # Alternarive file to be executed in an elevated PowerShell instance.
+    [Parameter(Mandatory = $true, ParameterSetName='ScriptFile')] [String] $FilePath,
+    
+    [Parameter(Mandatory = $false)] [System.Object[]] $ArgumentList,
+    
+    [Parameter(ParameterSetName='ScriptBlock')] [switch] $LoadProfile,
+    [Parameter(ParameterSetName='ScriptBlock')] [switch] $NoProfile,
+    
+    [Parameter()] [switch] $RunAsTrustedInstaller,
+    [Parameter()] [switch] $RunAsSystem,
+    [Parameter()] [switch] $ClearCache,
+    
+    [Parameter()] [switch] $NewWindow,
+    [Parameter()] [switch] $KeepNewWindowOpen,
+    [Parameter()] [switch] $KeepShell,
+    [Parameter()] [switch] $NoKeep,
+    
+    [ValidateSet('Low', 'Medium', 'MediumPlus', 'High', 'System')]
+    [System.String]$Integrity,
 
-	[Parameter()]
-	[switch]
-	$LoadProfile = $false,
-
-	[Parameter()]
-	[switch]
-	$NoProfile = $false,
-	
-	[Parameter()]
-	[System.Management.Automation.PSCredential]
-	$Credential
+    [Parameter()] [System.Management.Automation.PSCredential] $Credential,
+    [Parameter(ValueFromPipeline)] [pscustomobject] $InputObject
 )
-
-# Replaces $using:variableName with the serialized value of $variableName.
-# Credit: https://stackoverflow.com/a/60583163/97471
-Function Serialize-Scriptblock
-{ 	
-    param(
-        [scriptblock]$Scriptblock
-    )
-    $rxp = '(?<!`)\$using:(?<var>\w+)'
-    $ssb = $Scriptblock.ToString()
-    $cb = {
-        $v = (Get-Variable -Name $args[0].Groups['var'] -ValueOnly)
-		if ($v -eq $null)
-		{ '$null' }
-		else 
-		{ 
-			"`$([System.Management.Automation.PSSerializer]::Deserialize([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('{0}'))))" -f [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes([System.Management.Automation.PSSerializer]::Serialize($v)))
-		}		
+Begin {
+    $inputArray = @() 
+}
+Process {
+    foreach ($item in $InputObject) {
+        # Add the modified item to the output array
+        $inputArray += $item
     }
-    $sb = [RegEx]::Replace($ssb, $rxp, $cb, [System.Text.RegularExpressions.RegexOptions]::IgnoreCase);
-	return $sb;
 }
+End {
+    $gsudoArgs = @()
 
-Function Deserialize-Scriptblock
-{
-    param(
-		[string]$sb
-    )
-    [Scriptblock]::Create($sb).GetNewClosure()
-}
+    if ($PSCmdlet.MyInvocation.BoundParameters["Debug"].IsPresent) { $gsudoArgs += '--debug' }
 
-$expectingInput = $myInvocation.expectingInput;
-$debug = if ($PSBoundParameters['Debug']) {$true} else {$false};
-$userScriptBlock = Serialize-Scriptblock $ScriptBlock
-$InputArray = $Input
-$location = (Get-Location).Path;
+    if ($LoadProfile)	{ $gsudoArgs += '--LoadProfile' }
+    if ($RunAsTrustedInstaller)	{ $gsudoArgs += '--ti' }
+    if ($RunAsSystem)	{ $gsudoArgs += '-s' }
+    if ($ClearCache)	{ $gsudoArgs += '-k' }
+    if ($NewWindow)		{ $gsudoArgs += '-n' }
+    if ($KeepNewWindowOpen)		{ $gsudoArgs += '--KeepWindow' }
+    if ($NoKeep)		{ $gsudoArgs += '--close' }
+    if ($Integrity)	{ $gsudoArgs += '--integrity'; $gsudoArgs += $Integrity}
 
-if($PSBoundParameters["ErrorAction"]) {
-	#Write-Verbose -verbose "Received ErrorAction $($PSBoundParameters["ErrorAction"])"
-	$errorAction = $PSBoundParameters["ErrorAction"] | Out-String
-} else {
-	#Write-Verbose -verbose "ErrorActionPreference is $ErrorActionPreference"
-	$errorAction = $ErrorActionPreference | Out-String
-}
+    if ($Credential) {
+        $CurrentSid = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).User.Value;
+        $gsudoArgs += "-u", $credential.UserName
+        
+        # At the time of writing this, there is no way (considered secure) to send the password to gsudo. So instead of sending the password, lets start a credentials cache instance.	
+        $p = Start-Process "gsudo.exe" -Args "-u $($credential.UserName) gsudoservice $PID $CurrentSid All 00:05:00" -credential $Credential -LoadUserProfile -WorkingDirectory "$env:windir" -WindowStyle Hidden -PassThru 
+        $p.WaitForExit();
+        Start-Sleep -Seconds 1
+    } 
 
-$remoteCmd = Serialize-Scriptblock {
-	$InputObject = $using:InputArray;
-	$argumentList = $using:ArgumentList;
-	$expectingInput = $using:expectingInput;
-	$sb = [Scriptblock]::Create($using:userScriptBlock);
-	Set-Location $using:location;
-	$ErrorActionPreference=$using:errorAction;
+    if ($PSVersionTable.PSVersion.Major -le 5) {
+        $pwsh = "powershell.exe" 
+    } else 	{
+        $pwsh = "pwsh.exe" 
+    }
 
-	if ($expectingInput) { 
-		try { 
-			($InputObject | Invoke-Command $sb -ArgumentList $argumentList)
-		} catch {throw $_} 
-	} else { 
-		try{
-			(Invoke-Command $sb -ArgumentList $argumentList)
-		} catch {throw $_} 
-	} 
-}
+    if ($ScriptBlock) {	
+    
+        # Replace $using:variableName with the serialized value of $variableName.
+        # Credit: https://stackoverflow.com/a/60583163/97471
+        $rxp = '(?<!`)\$using:(?<var>\w+)'
+        $ssb = $ScriptBlock.ToString()
+        $cb = {
+            $v = (Get-Variable -Name $args[0].Groups['var'] -ValueOnly)
+            if ($v -eq $null) { 
+			'$null' 
+			} else  { 
+            "`$([System.Management.Automation.PSSerializer]::Deserialize([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('{0}'))))" -f [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes([System.Management.Automation.PSSerializer]::Serialize($v)))
+            }
+        }
+        $ScriptBlockWithUsings = [Scriptblock]::Create(
+				[RegEx]::Replace($ssb, $rxp, $cb, [System.Text.RegularExpressions.RegexOptions]::IgnoreCase));
+        ###############################
+        
+        if ($NoProfile) { 
+            $gsudoArgs += '-d';
+            $gsudoArgs += $pwsh;
+            $gsudoArgs += '-NoProfile';
+            $gsudoArgs += '-NoLogo';
+            
+            if ($KeepShell)		{ $gsudoArgs += '--NoExit' }
+        } else {
+            if ($KeepShell)		{ $gsudoArgs += '--KeepShell' }
+        }
 
-if ($Debug) {
-	Write-Debug "User ScriptBlock : $userScriptBlock"
-	Write-Debug "Full Script to run on the isolated instance: { $remoteCmd }" 
-} 
-
-$pwsh = ("$([System.Diagnostics.Process]::GetCurrentProcess().MainModule.FileName)") # Get same running powershell EXE.
-
-if ($host.Name -notmatch 'consolehost') { # Workaround for PowerShell ISE, or PS hosted inside other process
-	if ($PSVersionTable.PSVersion.Major -le 5) 
-		{ $pwsh = "powershell.exe" } 
-	else 
-		{ $pwsh = "pwsh.exe" }
-} 
-
-$windowTitle = $host.ui.RawUI.WindowTitle;
-$arguments = "-d", "--LogLevel", "Error"
-
-if ($credential) {
-	$currentSid = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).User.Value;
-	$user = "-u $($credential.UserName) "
-	$arguments += "-u", $credential.UserName
-	
-	# At the time of writing this, there is no way (considered secure) to send the password to gsudo. So instead of sending the password, lets start a credentials cache instance.	
-	Start-Process "gsudo.exe" -Args "$dbg -u $($credential.UserName) gsudoservice $PID $CurrentSid All 00:05:00" -credential $Credential -LoadUserProfile -WorkingDirectory "$env:windir" *> $null
-	# This may fail with `The specified drive root "C:\Users\gerar\AppData\Local\Temp\" either does not exist, or it is not a folder.` https://github.com/PowerShell/PowerShell/issues/18333
-	
-	#$p.WaitForExit();
-	Start-Sleep -Seconds 1
-} else {
-	$user = "";
-}
-
-if ($debug) { $arguments += "--debug"}
-
-$arguments += $pwsh
-
-if ($LoadProfile -and (-not $NoProfile -or (gsudo.exe --loglevel None config PowerShellLoadProfile).Split(" = ")[1] -like "*true*")) {
-} else {
-	$arguments += "-NoProfile"
-}
-
-$arguments += "-NoLogo", "-NonInteractive", "-OutputFormat", "Xml", "-InputFormat", "Text", "-encodedCommand", "IAAoACQAaQBuAHAAdQB0ACAAfAAgAE8AdQB0AC0AUwB0AHIAaQBuAGcAKQAgAHwAIABpAGUAeAAgAA=="
-
-# Must Read: https://stackoverflow.com/questions/68136128/how-do-i-call-the-powershell-cli-robustly-with-respect-to-character-encoding-i?noredirect=1&lq=1
-
-$result = $remoteCmd | & gsudo.exe @arguments *>&1
-
-$host.ui.RawUI.WindowTitle = $windowTitle;
-
-& {
-	Set-StrictMode -Off #within this scope
-
-	ForEach ($item in $result)
-	{
-		if (
-		$item.psobject.Properties['Exception'] -and
-		($item.Exception.SerializedRemoteException.WasThrownFromThrowStatement -or
-		 $item.Exception.WasThrownFromThrowStatement)
-		)
-		{
-			throw $item
-		}
-		if ($item -is [System.Management.Automation.ErrorRecord])
-		{ 
-			Write-Error $item
-		}
-		else 
-		{ 
-			Write-Output $item; 
-		}
-	}
+        if ($myInvocation.expectingInput) {
+            $inputArray | gsudo.exe @gsudoArgs $ScriptBlockWithUsings -args $ArgumentList
+        } else {
+            gsudo.exe @gsudoArgs $ScriptBlockWithUsings -args $ArgumentList
+        }
+    } else {
+        if ($myInvocation.expectingInput) {
+            $inputArray | gsudo.exe @gsudoArgs -args $ArgumentList
+        } else {
+            gsudo.exe @gsudoArgs -d $pwsh -File $FilePath -args $ArgumentList
+        }
+    }
 }
