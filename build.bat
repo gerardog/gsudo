f70cd98105fdaaa01358a996f13fc999f2b05bd5
Martí Climent <marticlilop@gmail.com>
2024-09-13 23:17:06 +0200

    Add build script

---
+++ build.bat
@@ -0,0 +1,17 @@
+@echo off
+
+pushd src\gsudo
+rmdir /q /s bin
+dotnet clean
+dotnet publish --nologo
+popd
+
+rmdir /q /s output
+mkdir output
+move "src\gsudo\bin\net8.0\win-x64\publish\UniGetUI Elevator.exe" output\
+move "src\gsudo\bin\net8.0\win-x64\publish\getfilesiginforedist.dll" output\
+
+%SIGNCOMMAND% "%cd%\output\UniGetUI` Elevator.exe"
+
+echo "Resulting files from build available at %cd%\output"
+pause
