e0ffad16e9e2c53fa8c4f97ba147c0c5c7c040cb
Gerardo Grignoli <gerardog@gmail.com>
2025-02-16 15:37:38 -0300

    Fixes for Net 8 & NativeAot build

--- build/01-build.ps1
+++ build/01-build.ps1
@@ -14,9 +14,9 @@ if ($env:version) {
 "-- Cleaning bin & obj folders"
 Get-Item ".\src\gsudo\bin\", ".\src\gsudo\obj\" -ErrorAction Ignore | Remove-Item -Recurse -Force
 "-- Building net8.0 win-arm64"
-dotnet publish .\src\gsudo\gsudo.csproj -c Release -o .\artifacts\arm64  -f net8.0 -r win-arm64 --sc -p:PublishAot=true -p:IlcOptimizationPreference=Size -v minimal -p:WarningLevel=0 || $(exit $LASTEXITCODE)
+dotnet publish .\src\gsudo\gsudo.csproj -c Release -o .\artifacts\arm64  -f net8.0 -r win-arm64 --sc -p:IlcOptimizationPreference=Size -v minimal -p:WarningLevel=0 || $(exit $LASTEXITCODE)
 "-- Building net8.0 win-x64"
-dotnet publish .\src\gsudo\gsudo.csproj -c Release -o .\artifacts\x64    -f net8.0 -r win-x64   --sc -p:PublishAot=true -p:IlcOptimizationPreference=Size -v minimal -p:WarningLevel=0 || $(exit $LASTEXITCODE)
+dotnet publish .\src\gsudo\gsudo.csproj -c Release -o .\artifacts\x64    -f net8.0 -r win-x64   --sc -p:IlcOptimizationPreference=Size -v minimal -p:WarningLevel=0 || $(exit $LASTEXITCODE)
 "-- Building net8.0 win-x86"
 dotnet publish .\src\gsudo\gsudo.csproj -c Release -o .\artifacts\x86    -f net8.0 -r win-x86   --sc -p:PublishReadyToRun=true -p:PublishSingleFile=true -v minimal -p:WarningLevel=0 || $(exit $LASTEXITCODE)
 "-- Building net4.6 AnyCpu"
