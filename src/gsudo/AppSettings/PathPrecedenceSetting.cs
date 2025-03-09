2c4c0c62387e2b3dc2daa29e34a994b873a30cd0
Gerardo Grignoli <gerardog@gmail.com>
2025-03-09 17:27:46 -0300

    `gsudo config PathPrecedence true` creates a sudo Symlink in `gsudo.exe` folder to ensure it takes precedence even when installed via scoop or portable. Updated docs. Fixes #387

--- src/gsudo/AppSettings/PathPrecedenceSetting.cs
+++ src/gsudo/AppSettings/PathPrecedenceSetting.cs
@@ -20,46 +20,85 @@ namespace gsudo.AppSettings
 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="newValue"></param>
+        /// <param name="global"></param>
         public override void Save(string newValue, bool global)
         {
-            bool bNewValue = bool.Parse(newValue);
-            var ourPath = Path.GetDirectoryName(ProcessFactory.FindExecutableInPath("gsudo.exe")) // shim
+            bool shouldPrioritizeGsudo = bool.Parse(newValue); // true = Prioritize gsudo, false = xde-prioritize gsudo in favor of system32
+
+            var ourPath = Path.GetDirectoryName(ProcessFactory.FindExecutableInPath("gsudo.exe"))
                             ?? Path.GetDirectoryName(ProcessHelper.GetOwnExeName());
 
+            AdjustPathOrder(shouldPrioritizeGsudo, ourPath);
+
+            CreateSudoSymLinkIfNeeded(shouldPrioritizeGsudo, ourPath);
+
+            // Save the value to the registry
+            base.Save(newValue, global);
+
+            // Notify the system of the change
+            string environment = "Environment";
+            IntPtr lParam = Marshal.StringToHGlobalUni(environment);
+            SendNotifyMessage(HWND_BROADCAST, WM_SETTINGCHANGE, UIntPtr.Zero, lParam);
+            Marshal.FreeHGlobal(lParam);
+        }
+
+        private static void CreateSudoSymLinkIfNeeded(bool shouldPrioritizeGsudo, string ourPath)
+        {
+            // Create a Symbolic link to sudo.exe, if none in the path exists
+            if (shouldPrioritizeGsudo)
+            {
+                // Detect if other sudo.exe still first in the path:
+#if NET9_0_OR_GREATER
+                string estimatedNewPath = $"{Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine)};{Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User)}";
+                var actualPath = Path.GetDirectoryName(ProcessFactory.FindExecutableInPath("sudo.exe", estimatedNewPath));
+                if (actualPath != ourPath)
+                {
+                    // Create the symbolic link
+                    Logger.Instance.Log($"A symbolic link \"sudo.exe\" will be created at \"{ourPath}\" to point to gsudo.exe.", LogLevel.Warning);
+                    File.CreateSymbolicLink(Path.Combine(ourPath, "sudo.exe"), Path.Combine(ourPath, "gsudo.exe"));
+                }
+#endif
+            }
+            Logger.Instance.Log("Please restart all your consoles to ensure the change makes effect.", LogLevel.Warning);
+        }
+
+        private static void AdjustPathOrder(bool shouldPrioritizeGsudo, string ourPath)
+        {
             var system32Path = Environment.GetFolderPath(Environment.SpecialFolder.System);
 
+            // Calculate the new PATH
             var allPaths = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine).Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
-                // I could also do .Distinct(StringComparer.OrdinalIgnoreCase);
-                // ...and it works well on local, but may be out of our responsibility to fix that.
-                
+
             IEnumerable<string> newPath;
 
-            if (bNewValue)
+            if (shouldPrioritizeGsudo)
                 newPath = new[] { ourPath }.Concat(allPaths.Where(p => !p.Equals(ourPath, StringComparison.OrdinalIgnoreCase)));
             else
                 newPath = allPaths.Where(p => !p.Equals(ourPath, StringComparison.OrdinalIgnoreCase)).Concat(new[] { ourPath });
 
             var finalStringPath = string.Join(";", newPath);
 
+            // Update the PATH
             Logger.Instance.Log($"Updating PATH environment variable to: {finalStringPath}", LogLevel.Debug);
 
             Environment.SetEnvironmentVariable("Path", finalStringPath, EnvironmentVariableTarget.Machine);
-            base.Save(newValue, global);
 
-            if (bNewValue)
+            if (shouldPrioritizeGsudo)
+            {
                 Logger.Instance.Log($"\"{ourPath}\" path is now prioritized in the PATH environment variable.", LogLevel.Info);
+            }
             else
+            {
                 Logger.Instance.Log($"\"{system32Path}\" path is now prioritized in the PATH environment variable.", LogLevel.Info);
-
-            Logger.Instance.Log("Please restart all your consoles to ensure the change makes effect.", LogLevel.Warning);
-
-            // Notify the system of the change
-            SendNotifyMessage(HWND_BROADCAST, WM_SETTINGCHANGE, UIntPtr.Zero, "Environment");
+            }
         }
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        private static extern bool SendNotifyMessage(
-            IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern bool SendNotifyMessage(IntPtr hWnd, uint Msg, UIntPtr wParam, IntPtr lParam);
 
         private const uint WM_SETTINGCHANGE = 0x001A;
         private static readonly IntPtr HWND_BROADCAST = new IntPtr(0xFFFF);
